37715043ffabe0ed3bc817db17c9d3312d29cb79
migi0401 <migi111@naver.com>
2026-02-10 12:26:59 +0900

    Feat: Add ProxyMode

--- src/main/java/hello/core/web/LogDemoService.java
+++ src/main/java/hello/core/web/LogDemoService.java
@@ -2,6 +2,9 @@ package hello.core.web;
 
 import hello.core.common.MyLogger;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,4 +16,9 @@ public class LogDemoService {
     public void logic(String id) {
         myLogger.log("service id = " + id);
     }
-}
+}@Scope(value = "request", proxyMode = ScopedProxyMode.TARGET_CLASS)
+public class MyLogger {
+
+    private String uuid;
+    private String requestURL;
+
